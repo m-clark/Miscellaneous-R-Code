dc1f3a9bcfd8133543153f95a7aefe9ab102c86a
mc <6876c76f@opayq.com>
2013-11-17 23:15:38 -0500

    correct comment

--- ModelFitting/gp Examples/gaussianprocessNoiseFree.R
+++ ModelFitting/gp Examples/gaussianprocessNoiseFree.R
@@ -1,6 +1,6 @@
 ########################################################################################
-### 'Noisy' gaussian process demo.  The matrix labeling is in keeping with Murphy    ###
-### 2012 and Rasmussen and Williams 2006.  See those sources for more detail.        ###
+### 'Noise-free' gaussian process demo.  The matrix labeling is in keeping with      ###
+### Murphy 2012 and Rasmussen and Williams 2006.  See those sources for more detail. ###
 ### Murphy's matlab code can be found here: https://code.google.com/p/pmtk3/, though ###
 ### the relevant files are housed alongside this code.                               ###
 ###                                                                                  ###
