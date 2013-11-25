5311b0e4f5ac5fb0012c26f479b679c45b276fdb
mc <6876c76f@opayq.com>
2013-11-24 20:59:50 -0500

    add zis

---
+++ ModelFitting/NBzeroinfl.R
@@ -0,0 +1,112 @@
+##########################################################################################################
+### Log likelihood function to estimate parameters for a Zero-inflated Negative Binomial model. With   ###
+### examples and comparison to pscl package output. Also includes approach based on Hilbe GLM text.    ###
+### see also: https://github.com/mclark--/Miscellaneous-R-Code/blob/master/ModelFitting/poiszeroinfl.R ###
+##########################################################################################################
+
+ZINB = function(y, X, par) {
+  # arguments are response y, predictor matrix X, and parameter named starting points of 'logit', 'negbin', and 'theta'
+  
+  # Extract parameters
+  logitpars = par[grep('logit', names(par))]
+  negbinpars = par[grep('negbin', names(par))]
+  theta = exp(par[grep('theta', names(par))])
+  
+  # Logit part; in this function Xlogit = Xnegbin but one could split X argument into Xlogit and Xnegbin for example
+  Xlogit = X
+  LPlogit = Xlogit%*%logitpars
+  logi0 = plogis(LPlogit) 
+  
+  # Negbin part
+  Xnegbin = X
+  munb = exp(Xnegbin%*%negbinpars)
+  
+  # LLs
+  logliklogit = log( logi0 + exp(log(1-logi0) + suppressWarnings(dnbinom(0, size=theta, mu=munb, log=T))) )
+  logliknegbin = log(1-logi0) + suppressWarnings(dnbinom(y, size=theta, mu=munb, log=T))
+  
+  ### Hilbe formulation
+  # theta part 
+  # alpha = 1/theta  
+  # m = 1/alpha
+  # p = 1/(1 + alpha*munb)
+  
+  # logliklogit = log( logi0 + (1 - logi0)*(p^m) )
+  # logliknegbin = log(1-logi0) + log(gamma(m+y)) - log(gamma(m)) + m*log(p) + y*log(1-p)   # gamma(y+1) not needed
+  
+  y0 = y==0   # 0 values
+  yc = y>0    # Count part
+  
+  loglik = -( sum(logliklogit[y0]) + sum(logliknegbin[yc]) )
+  loglik
+}
+
+
+### Get the data ###
+library(foreign)
+fish = read.dta("http://www.stata-press.com/data/r11/fish.dta")
+
+
+### Get starting values or simply do zeros ###
+# for this function, a named vector for the starting values; for zinb: 'logit', 'negbin', 'theta';
+init.mod = model.matrix(count ~ persons + livebait, data=fish) # to get X matrix
+
+startlogi = glm((count==0) ~ persons + livebait, data=fish, family="binomial")
+startcount = glm(count ~ persons + livebait, data=fish, family="poisson")
+
+starts = c(negbin=coef(startcount), logit=coef(startlogi), theta=1)  
+# starts = c(negbin=rep(0, 3), logit=rep(0, 3), theta=log(1))  
+
+
+### Estimate with optim function ###
+optNB1 = optim(par=starts , fn=ZINB, X=init.mod, y=fish$count, 
+               method="BFGS", control=list(maxit=5000, reltol=1e-12), hessian=T)
+# optNB1
+
+### Comparison ###
+# Extract for clean display
+B = optNB1$par
+se = sqrt(diag(solve((optNB1$hessian))))
+Z = B/se
+p = pnorm(abs(Z), lower=F)*2
+
+# pscl results
+library(pscl)
+zinbmod1 = zeroinfl(count~persons + livebait, data=fish, dist="negbin")  
+summary(zinbmod1)
+round(data.frame(B, se, Z, p), 4)  # note that theta here is actually log(theta)
+
+#########################
+### Optional data set ###
+#########################
+
+### Get the data ###
+data("bioChemists", package = "pscl")
+
+### Get starting values or simply do zeros
+init.mod = model.matrix(art ~ fem + mar + kid5 + phd + ment, data=bioChemists) # to get X matrix
+startlogi = glm((art==0) ~ fem + mar + kid5 + phd + ment, data=bioChemists, family="binomial")
+startcount = glm(art ~ fem + mar + kid5 + phd + ment, data=bioChemists, family="quasipoisson")
+
+starts = c(negbin=coef(startcount), logit=coef(startlogi), theta=summary(startcount)$dispersion)  
+# starts = c(negbin=rep(0, 6), logit=rep(0, 6), theta=1)
+
+
+### Estimate with optim function ###
+optNB2 = optim(par=starts , fn=ZINB, X=init.mod, y=bioChemists$art, 
+               method="BFGS", control=list(maxit=5000, reltol=1e-12), hessian=T)
+# optNB2
+
+
+### Comparison ###
+# Extract for clean display.
+B = optNB2$par
+se = sqrt(diag(solve((optNB2$hessian))))
+Z = B/se
+p = pnorm(abs(Z), lower=F)*2
+
+# pscl results
+library(pscl)
+zinbmod = zeroinfl(art ~ . | ., data = bioChemists, dist = "negbin")
+summary(zinbmod)
+round(data.frame(B,se, Z, p), 4)
---
+++ ModelFitting/poiszeroinfl.R
@@ -0,0 +1,69 @@
+########################################################################################################
+### Log likelihood function to estimate parameters for a Zero-inflated Poisson model. With examples  ###
+### and comparison to pscl package output. Also includes approach based on Hilbe GLM text.           ###
+### see also: https://github.com/mclark--/Miscellaneous-R-Code/blob/master/ModelFitting/NBzeroinfl.R ###
+########################################################################################################
+
+ZIP = function(y, X, par) {
+  # arguments are response y, predictor matrix X, and parameter named starting points of 'logit' and 'pois'
+  
+  # Extract parameters
+  logitpars = par[grep('logit', names(par))]   
+  poispars = par[grep('pois', names(par))]     
+  
+  # Logit part; in this function Xlogit = Xpois but one could split X argument into Xlogi and Xpois for example
+  Xlogit = X
+  LPlogit = Xlogit%*%logitpars
+  logi0 = plogis(LPlogit)  # alternative 1/(1+exp(-LPlogit))
+    
+  # Poisson part
+  Xpois = X
+  mupois = exp(Xpois%*%poispars)
+  
+  # LLs
+  logliklogit = log( logi0 + exp(log(1-logi0) - mupois) )
+  loglikpois = log(1-logi0) + dpois(y, lambda=mupois, log=T)
+  
+  # Hilbe formulation
+  # logliklogit = log(logi0 + (1 - logi0)*exp(- mupois) )
+  # loglikpois = log(1-logi0) -mupois + log(mupois)*y     #not necessary: - log(gamma(y+1))
+    
+  y0 = y==0  # 0 values
+  yc = y>0   # Count part
+
+  loglik = -( sum(logliklogit[y0]) + sum(loglikpois[yc]) )
+  loglik
+}
+
+### Get the data ###
+library(foreign); library(pscl)
+fish = read.dta("http://www.stata-press.com/data/r11/fish.dta")
+
+
+### Get starting values or simply do zeros ###
+# for this function, a named vector for the starting values; for zip: need 'logit', 'pois'
+init.mod = glm(count ~ persons + livebait, data=fish, x=T, y=T, "poisson")
+
+# starts = c(logit=coef(init.mod), pois=coef(init.mod))  
+starts = c(rep(0, 3), rep(0, 3))
+names(starts) = c( paste0('pois.', names(coef(init.mod))), paste0('logit.', names(coef(init.mod))) )
+                                                               
+
+### Estimate with optim function ###
+optPois1 = optim(par=starts , fn=ZIP, X=init.mod$x, y=init.mod$y, 
+                 method="BFGS", control=list(maxit=5000, reltol=1e-12), 
+                 hessian=T)
+# optPois1
+
+
+### Comparison ###
+# Extract for clean display
+B = optPois1$par
+se = sqrt(diag(solve((optPois1$hessian))))
+Z = B/se
+p = pnorm(abs(Z), lower=F)*2
+
+# pscl results
+zipoismod = zeroinfl(count ~ persons + livebait, data=fish, dist="poisson") 
+summary(zipoismod)
+round(data.frame(B, se, Z, p), 4)
