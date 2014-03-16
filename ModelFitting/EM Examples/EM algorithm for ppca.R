52a8dda8621ee97269a0d99f4f6dd89892129a86
mc <6876c76f@opayq.com>
2014-03-16 13:02:48 -0400

    added ppca

---
+++ ModelFitting/EM Examples/EM algorithm for ppca.R
@@ -0,0 +1,132 @@
+# ----------------------------------------------------------------------------------#
+# The following is an EM algorithm for probabilistic principal components analysis. #
+# Based on Tipping and Bishop, 1999, and also Murphy 2012 Probabilistic ML.  See    #
+# also ModelFitting/EM Examples/EM for pca.R                                        #
+# ----------------------------------------------------------------------------------#
+
+#####################
+### Main Function ###
+#####################
+
+PPCAEM = function(X, nComp=2, tol=.00001, maxits=100, showits=T){
+  # Arguments X: numeric data, nComp: number of components
+  # tol = tolerance level, maxits: maximum iterations, showits: show iterations
+  require(pracma) # for orthonormal basis of W; pcaMethods package has also
+  require(psych)  # for tr
+  
+  # starting points and other initializations
+  N = nrow(X)
+  D = ncol(X)
+  L = nComp
+
+  S = (1/N) * t(X)%*%X
+  evals = eigen(S)$values
+  evecs = eigen(S)$vectors
+
+  V = evecs[,1:L]
+  Lambda = diag(evals[1:L])
+  
+  Z = t(replicate(L, rnorm(N)))                                    # latent variables
+  sigma2 = 1/(D-L) * sum(evals[(L+1):D])                           # variance; average variance associated with discarded dimensions
+  W = V %*% chol(Lambda-sigma2*diag(L)) %*% diag(L)                # loadings; this and sigma2 starting points will be near final estimate
+
+  it = 0
+  converged = FALSE
+  ll = 0
+  
+  if (showits)                                                     # Show iterations
+    cat(paste("Iterations of EM:", "\n"))
+  while ((!converged) & (it < maxits)) {                           # while no convergence and we haven't reached our max iterations do this stuff 
+    # create 'old' values for comparison
+    if(exists('W.new')){
+      W.old = W.new
+    }
+    else {
+      W.old = W
+    }
+    
+    ll.old = ll
+    
+    Psi = sigma2*diag(L)
+    M = t(W.old) %*% W.old + Psi
+    
+    W.new = S%*%W%*%solve(Psi + solve(M)%*%t(W.old)%*%S%*%W.old)   # E and M together
+    sigma2 = 1/D * tr(S - S%*%W.old%*%solve(M)%*%t(W.new))
+
+    Z = solve(M)%*%t(W.new)%*%t(X)
+    ZZ = sigma2*solve(M) + Z%*%t(Z)
+    
+    # log likelihood as in paper
+#     ll = .5*sigma2*D + .5*tr(ZZ) + .5*sigma2 * X%*%t(X) -
+#          1/sigma2 * t(Z)%*%t(W.new)%*%t(X) + .5*sigma2 * tr(t(W.new)%*%W.new%*%ZZ)
+#     ll = -sum(ll)
+    
+    # more straightforward
+    ll = dnorm(X, mean=t(W.new%*%Z), sd=sqrt(sigma2), log=T)
+    ll = -sum(ll)
+
+    it = it + 1
+    if (showits & (it == 1 | it%%5 == 0))                          # if showits, show first and every 5th iteration
+      cat(paste(format(it), "...", "\n", sep = ""))
+    converged = max(abs(ll.old-ll)) <= tol
+  }
+  
+  W = orth(W)
+  evs = eigen(cov(X %*% W))
+  evecs = evs$vectors
+  
+  W = W %*% evecs
+  Z = X %*% W
+  Xrecon = Z %*% t(W)
+  reconerr = sum((Xrecon-X)^2)
+  
+  if (showits)                                                     # Show last iteration
+    cat(paste0(format(it), "...", "\n"))
+  
+  return(list(scores=Z, loadings=W, Xrecon=Xrecon, reconerr = reconerr, ll=ll, sigma2=sigma2))
+}
+
+
+
+###############
+### Example ###
+###############
+
+### Get data and run
+# state.x77 is the data; various state demographics
+X = scale(state.x77)
+outEM = PPCAEM(X=X, nComp=2, tol=1e-12, maxit=100)
+outEM  
+
+# Examine reconstructed values vs. original
+Xrecon = outEM$Xrecon
+mean((Xrecon-X)^2)  
+
+
+#################################################
+### compare results to output from pcaMethods ###
+#################################################
+
+# note that signs for loadings/scores may be opposite
+library(pcaMethods)
+outpcam = ppca(X, nPcs=2, threshold=1e-3)
+
+# compare loadings and scores
+cbind(loadings(outpcam), outEM$loadings)
+sum((abs(loadings(outpcam))-abs(outEM$loadings))^2)
+abs(round(cbind(scores(outpcam), outEM$scores), 2))
+# abs(scores(outpcam)) - abs(scores(pca(X,n=2))) # ppca is pretty close to reproducing pca
+
+# compare reconstructed data sets
+Xrecon2 = scores(outpcam) %*% t(loadings(outpcam))
+mean((Xrecon2-X)^2)
+mean(abs(Xrecon2-Xrecon))
+
+# plots
+library(car)
+scatterplotMatrix(cbind(X[,1],Xrecon[,1], Xrecon2[,1]))
+scatterplotMatrix(cbind(X[,2],Xrecon[,2], Xrecon2[,2]))
+
+plot(Xrecon[,1], Xrecon2[,1], pch=19, col='gray50')
+
+scatterplotMatrix(cbind(outEM$scores, outpcam@scores) )
