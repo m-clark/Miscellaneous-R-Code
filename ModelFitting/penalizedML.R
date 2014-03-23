0014e8e1c4d880a695f3938e8bebd039743e5e75
mc <6876c76f@opayq.com>
2014-03-22 20:18:18 -0400

    add lm and penalized approach

---
+++ ModelFitting/penalizedML.R
@@ -0,0 +1,130 @@
+#---------------------------------------------------------------------------------#
+# A standard regression model via penalized likelihood.  See the standardlm.R     #
+# code for comparison. Here the penalty is specified (via lambda argument) but    # 
+# one would typically estimate via cross-validation or some other fashion. Two    #
+# penalties are possible with the function.  One using the (squared) L2 norm (aka #
+# ridge regression, tikhonov regularization), another using the L1 norm (aka      #
+# lasso) which has the possibility of penalizing coefficients to zero, and thus   #
+# can serve as a model selection procedure.                                       #
+#                                                                                 #
+# Note that both L2 and L1 approaches can be seen as maximum a posteriori (MAP)   #
+# estimates for a Bayesian regression with a specific prior on the coefficients.  #
+# The L2 approach is akin to a normal prior with zero mean, while L1 is akin to   #
+# a zero mean Laplace prior.  See the Bayesian scripts for ways to implement.     #
+#---------------------------------------------------------------------------------#
+
+
+##############
+# Data Setup #
+##############
+set.seed(123)  # ensures replication
+
+# predictors and response
+N = 100 # sample size
+k = 2   # number of desired predictors
+X = matrix(rnorm(N*k), ncol=k)  
+y = -.5 + .2*X[,1] + .1*X[,2] + rnorm(N, sd=.5)  # increasing N will get estimated values closer to these
+
+dfXy = data.frame(X,y)
+
+
+
+#############
+# Functions #
+#############
+# A maximum likelihood approach
+penalML = function(par, X, y, lambda=.1, type='L2'){
+  # arguments- par: parameters to be estimated; X: predictor matrix with
+  # intercept column; y: response, lambda: penalty coefficient; type: penalty
+  # approach
+  
+  # setup
+  beta = par[-1]                               # coefficients
+  sigma2 = par[1]                              # error variance
+  sigma = sqrt(sigma2)
+  N = nrow(X)
+  
+  # linear predictor
+  LP = X%*%beta                                # linear predictor
+  mu = LP                                      # identity link in the glm sense
+  
+  # calculate likelihood
+  L = dnorm(y, mean=mu, sd=sigma, log=T)       # log likelihood
+  
+  PL = switch(type,
+              'L2' = -sum(L) + lambda*crossprod(beta[-1]),   # the intercept is not penalized
+              'L1' = -sum(L) + lambda*sum(abs(beta[-1]))
+              )
+}
+
+
+# glmnet style approach that will put the lambda coefficient on equivalent
+# scale; Uses a different objective function.  Note that glmnet is actually
+# 'elasticnet' and mixes both L1 and L2 penalties
+
+penalML2 = function(par, X, y, lambda=.1, type='L2'){
+  # arguments- par: parameters to be estimated; X: predictor matrix with
+  # intercept column; y: response, lambda: penalty coefficient; type: penalty
+  # approach
+  
+  # setup
+  beta = par                                   # coefficients
+  N = nrow(X)
+  
+  # linear predictor
+  LP = X%*%beta                                # linear predictor
+  mu = LP                                      # identity link in the glm sense
+  
+  obj = switch(type,
+               'L2' = .5*crossprod(y-X%*%beta)/N + lambda*crossprod(beta[-1]),
+               'L1' = .5*crossprod(y-X%*%beta)/N + lambda*sum(abs(beta[-1]))
+  )
+}
+
+
+
+##############################
+### Obtain Model Estimates ###
+##############################
+### Setup for use with optim
+X = cbind(1, X)
+
+# initial values; note we'd normally want to handle the sigma differently as it's
+# bounded by zero, but we'll ignore for demonstration; also sigma2 is not required
+# for the LS approach
+init = c(1, rep(0, ncol(X)));  names(init)=c('sigma2', 'intercept','b1', 'b2')
+
+optlmpenalMLL2 = optim(par=init, fn=penalML, X=X, y=y, lambda=1, control=list(reltol=1e-12))
+optlmpenalMLL1 = optim(par=init, fn=penalML, X=X, y=y, lambda=1, type='L1', control=list(reltol=1e-12))
+
+parspenalMLL2 = optlmpenalMLL2$par
+parspenalMLL1 = optlmpenalMLL1$par
+
+
+
+##################
+### Comparison ###
+##################
+### compare to lm
+modlm = lm(y~., dfXy)
+
+round(rbind(parspenalMLL2,
+            parspenalMLL1, 
+            modlm = c(summary(modlm)$sigma^2, coef(modlm))), 4)
+
+### compare to glmnet; setting alpha to 0 and 1 is equivalent L2 and L1 respectively; you also wouldn't want to specify lambda normally, and rather let it come about as part of the estimation procedure.  We do so here just for demonstration.
+library(glmnet)
+glmnetL2 = glmnet(X[,-1], y, alpha=0, lambda=.01, standardize=F)
+glmnetL1 = glmnet(X[,-1], y, alpha=1, lambda=.01, standardize=F)
+
+
+round(rbind(t(as.matrix(coef(glmnetL2))), 
+            functionoutput=optim(par=init[-1], fn=penalML2, X=X, y=y, 
+                                 lambda=.01, control=list(reltol=1e-12))$par), 
+      4)
+
+round(rbind(t(as.matrix(coef(glmnetL1))), 
+            functionoutput=optim(par=init[-1], fn=penalML2, X=X, y=y, 
+                                 lambda=.01, type='L1', control=list(reltol=1e-12))$par), 
+      4)
+
---
+++ ModelFitting/standardlm.R
@@ -0,0 +1,117 @@
+#------------------------------------------------------------------------------#
+# A standard regression model via maximum likelihood or least squares loss.    #
+# Can serve as an entry point for those starting out to the wider world of     #
+# computational statistics as maximum likelihood is the fundamental approach   #
+# used in most applied statistics, but which is also a key aspect of the       #
+# Bayesian approach.  Least squares loss is not confined to the standard lm    #
+# setting, but is widely used in more predictive/'algorithmic' approaches e.g. #
+# in machine learning and elsewhere.                                           #
+#------------------------------------------------------------------------------#
+
+
+##############
+# Data Setup #
+##############
+set.seed(123)  # ensures replication
+
+# predictors and response
+N = 100 # sample size
+k = 2   # number of desired predictors
+X = matrix(rnorm(N*k), ncol=k)  
+y = -.5 + .2*X[,1] + .1*X[,2] + rnorm(N, sd=.5)  # increasing N will get estimated values closer to these
+
+dfXy = data.frame(X,y)
+
+
+
+#############
+# Functions #
+#############
+# A maximum likelihood approach
+lmfuncML = function(par, X, y){
+  # arguments- par: parameters to be estimated; X: predictor matrix with intercept 
+  # column; y: response
+  
+  # setup
+  beta = par[-1]                               # coefficients
+  sigma2 = par[1]                              # error variance
+  sigma = sqrt(sigma2)
+  N = nrow(X)
+  
+  # linear predictor
+  LP = X%*%beta                                # linear predictor
+  mu = LP                                      # identity link in the glm sense
+  
+  # calculate likelihood
+  L = dnorm(y, mean=mu, sd=sigma, log=T)       # log likelihood
+#   L =  -((N/2)*log(sigma2)) - (1/(2*sigma2))*crossprod(y-mu)    # alternate, log likelihood form
+
+  -sum(L)                                      # optim by default is minimization, and we want to maximize the likelihood 
+                                               # (see also fnscale in optim.control)
+}
+
+# An equivalent approach via least squares loss function
+lmfuncLS = function(par, X, y){
+  # arguments- par: parameters to be estimated; X: predictor matrix with intercept 
+  # column, y: response
+  
+  # setup
+  beta = par                               # coefficients
+  
+  # linear predictor
+  LP = X%*%beta                                # linear predictor
+  mu = LP                                      # identity link
+  
+  # calculate least squares loss function
+  L = crossprod(y-mu)                        
+                                     
+}
+
+
+
+##############################
+### Obtain Model Estimates ###
+##############################
+# Setup for use with optim
+X = cbind(1, X)
+
+# initial values; note we'd normally want to handle the sigma differently as
+# it's bounded by zero, but we'll ignore for demonstration; also sigma2 is not
+# required for the LS approach as it is the objective function.
+init = c(1, rep(0, ncol(X)));  names(init)=c('sigma2', 'intercept','b1', 'b2')
+
+optlmML = optim(par=init, fn=lmfuncML, X=X, y=y, control=list(reltol=1e-8))
+optlmLS = optim(par=init[-1], fn=lmfuncLS, X=X, y=y, control=list(reltol=1e-8))
+parsML = optlmML$par
+parsLS = c(sigma2=optlmLS$value/(N-k-1), optlmLS$par)  # calculate sigma2 and add
+
+
+
+##################
+### Comparison ###
+##################
+### compare to lm which uses QR decomposition
+modlm = lm(y~., dfXy)
+
+round(rbind(parsML,
+            parsLS,
+            modlm = c(summary(modlm)$sigma^2, coef(modlm))), 3)
+
+# the slight difference in sigma is roughly maxlike dividing by N vs. N-1 in the
+# traditional least squares approach; diminishes with increasing N as both tend 
+# toward whatever sd^2 you specify when creating the y response above
+
+
+### compare to glm; by default assumes gaussian family with identity link and uses lm.fit
+modglm = glm(y~., data=dfXy)
+summary(modglm)
+
+
+### via normal equations
+coefs = solve(t(X)%*%X) %*% t(X)%*%y  # coefficients
+
+# compare
+sqrt(crossprod(y-X%*%coefs)/(N - k - 1)); summary(modlm)$sigma; sqrt(modglm$deviance/modglm$df.residual) 
+c(sqrt(parsML[1]), sqrt(parsLS[1]))
+
+# rerun by adding 3-4 zeros to the N
