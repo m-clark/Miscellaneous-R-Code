5d0aa4199637ab8551dac0b10041652db2f57cb8
m-clark <micl@umich.edu>
2017-05-13 13:40:21 -0400

    add augmented data approach

--- ModelFitting/ridge.R
+++ ModelFitting/ridge.R
@@ -20,8 +20,14 @@ y = scale(X %*% b + rnorm(N, sd=.5))
 
 # note, if lambda=0, result is lm.fit
 result_ridge = optim(rep(0, ncol(X)), ridge, X=X, y=y, lambda=.1, method='BFGS')
-result_ridge2 =  solve(crossprod(X) + diag(length(y)*.1, ncol(X))) %*% crossprod(X,y)
 
+# analytical
+result_ridge2 =  solve(crossprod(X) + diag(length(y)*.1, ncol(X))) %*% crossprod(X, y)
+
+# alternative with augmented data (note sigma ignored as =1, but otherwise X/sigma and y/sigma)
+X2 = rbind(X, diag(sqrt(length(y)*.1), ncol(X)))
+y2 = c(y, rep(0, ncol(X)))
+result_ridge3 = solve(crossprod(X2)) %*% crossprod(X2, y2)
 
 
 library(glmnet)
@@ -35,5 +41,6 @@ glmnet_res = coef(glmnet(X, y, alpha=0, lambda=c(10, 1, .1), thresh=1e-12, inter
 data.frame(lm=coef(lm(y~.-1, data.frame(X))),
            ridge=result_ridge$par,
            ridge2=result_ridge2,
+           ridge3=result_ridge3,
            glmnet = glmnet_res[-1, 1],
            truth=b)
